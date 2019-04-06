c1d75e689fdf450e9557cc39ce8e4621e819471d
993178 <papilio29@hotmail.com>
2019-04-07 01:09:48 +0200

    eerste dingetjes

---
+++ src/Poes.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './Poes.css';
+
+class Poes extends Component {
+  render() {
+    return (
+      <div className="Poes">
+        <p>Moet ik nou alles zelf typen? Ik ga lorem20 nog missen...</p>
+        <p>Hah, lang leve de shortcuts in een htmlbestand</p>
+      </div>
+    );
+  }
+}
+
+export default Poes;
