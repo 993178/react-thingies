df4c18eca6c1b45f7677a61cbf3aaf4b971e093d
993178 <papilio29@hotmail.com>
2019-09-05 19:03:22 +0200

    voorbeeld Suspense zonder BrowserRouter

--- http+ajaxverzoeken/React Suspense lazy routing/src/App.js
+++ http+ajaxverzoeken/React Suspense lazy routing/src/App.js
@@ -7,19 +7,33 @@ import Welcome from './containers/Welcome';
 const Posts = React.lazy(() => import('./containers/Posts')); // dynamische import met nieuwere lazy-methode die dus dat hele hoc-element in het Posts-project vervangt. Import kan alleen default exports ontvangen
 
 class App extends Component {
+  state = { showPosts: false }
+
+  modeHandler = () => {
+    this.setState(prevState => {
+      return { showPosts: !prevState.showPosts};
+    })
+  }
+
   render() {
-    return (
-      <BrowserRouter>
-        <React.Fragment> {/* dit doet hetzelfde als een aux: gewoon een wrapper die de inhoud zonder verandering teruggeeft zodat je meerdere elementen op hetzelfde niveau kunt renderen */}
-          <nav>
-            <NavLink to="/user">User Page</NavLink> |&nbsp;
-            <NavLink to="/posts">Posts Page</NavLink>
-          </nav>
-          <Route path="/" component={Welcome} exact />
-          <Route path="/user" component={User} />
-          <Route path="/posts" render={() => <Suspense fallback={<div>Loading...</div>}><Posts /></Suspense>} /> {/* Posts alleen indien nodig, Suspense is kennelijk de async binnen de JSX, met een Plan B voor als het niet onmiddellijk laadt */}
-        </React.Fragment>
-      </BrowserRouter>
+    return (  // voorbeeld van async rendering met Suspense dat niet binnen BrowserRouter staat. Werkt nog niet bij serverside rendering?
+      <React.Fragment>
+          <button onClick={this.modeHandler}>Toggle mode</button>
+          {this.state.showPosts ? (<Suspense fallback={<div>Loading...</div>}>
+            <Posts />
+          </Suspense>) : <User />}
+      </React.Fragment>
+      // <BrowserRouter>
+      //   <React.Fragment> {/* dit doet hetzelfde als een aux: gewoon een wrapper die de inhoud zonder verandering teruggeeft zodat je meerdere elementen op hetzelfde niveau kunt renderen */}
+      //     <nav>
+      //       <NavLink to="/user">User Page</NavLink> |&nbsp;
+      //       <NavLink to="/posts">Posts Page</NavLink>
+      //     </nav>
+      //     <Route path="/" component={Welcome} exact />
+      //     <Route path="/user" component={User} />
+      //     <Route path="/posts" render={() => <Suspense fallback={<div>Loading...</div>}><Posts /></Suspense>} /> {/* Posts alleen indien nodig, Suspense is kennelijk de async binnen de JSX, met een Plan B voor als het niet onmiddellijk laadt */}
+      //   </React.Fragment>
+      // </BrowserRouter>
     );
   }
 }
